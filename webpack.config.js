0c8a3182289dad524f5f9e963de8bd272e6e4b3f
GeekyHacks <geekyhacks22@gmail.com>
2023-08-09 09:56:36 +0300

    Add file-loader to load images to assets folder inside dist

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
       // this will load all images
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: [{ loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'assets/', publicPath: '/' } }],
       },
     ],
   },
